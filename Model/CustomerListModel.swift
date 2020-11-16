50d0815069a873e9ad510c4798af789972ba0895
SROO0524 <vmflstm847@naver.com>
2020-11-16 22:35:18 +0900

    Data model Set

--- Model/CustomerListModel.swift
+++ Model/CustomerListModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct CustomerList : Codable {
+    
+    let list : [List]
+    
+    struct List : Codable {
+        let custno: Int
+        let name : String
+        let contact : String
+        let birth : String
+        let memo: String
+        let dontsend: Int
+        let point : Int
+        let profile : Int
+        let chargeName : String
+        let regdate : String
+        let profileUrl : String
+        
+        enum CodingKeys: String, CodingKey {
+            case custno, name, contact, birth, memo, dontsend, point, profile, chargeName, regdate, profileUrl
+
+        }
+    }
+}
