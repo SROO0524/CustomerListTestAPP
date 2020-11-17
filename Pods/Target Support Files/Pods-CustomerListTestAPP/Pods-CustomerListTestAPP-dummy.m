3351b77dd36406c149fa103b55e9da41f8e8857b
SROO0524 <vmflstm847@naver.com>
2020-11-17 16:44:54 +0900

    Pod install

---
+++ Pods/Target Support Files/Pods-CustomerListTestAPP/Pods-CustomerListTestAPP-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CustomerListTestAPP : NSObject
+@end
+@implementation PodsDummy_Pods_CustomerListTestAPP
+@end
---
+++ Pods/Target Support Files/Pods-CustomerListTestAPP/Pods-CustomerListTestAPP-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_CustomerListTestAPPVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CustomerListTestAPPVersionString[];
+
